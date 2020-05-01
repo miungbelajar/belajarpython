092beef01f454f7c54c167c750370f81b3b2da76
miungbelajar <64505739+miungbelajar@users.noreply.github.com>
2020-05-01 12:19:30 +0700

    Create ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,58 @@
+nilai = 100
+
+if 90 <= nilai <= 100:
+    print("maaf anda tidak lulus,saya dosen killer anda bisa apa")
+else:
+    print("maaf anda tidak lulus mata kuliah ini")
+#c++
+# include <iostream>
+
+# using namespace std;
+
+# int main(){
+
+# 	int a;
+
+# 	cout << "masukan angka = ";
+# 	cin >> a;
+
+# 	if (a == 100){
+# 		cout << "nilai 100 anda belum lulus mata kuliah saya" << endl;
+# 	} else if (a == 90){
+# 		cout << "nilai 90 anda belum lulus" << endl;
+# 	} else if (a == 60){
+# 		cout << "nilai 60 anda lulus mata kuliah saya" << endl;
+# 	} else {
+# 		cout << "anda tidak lulus mata kuliah saya" << endl;
+# 	}
+
+# 	cout << "selesai" << endl;
+# 	cin.get();
+# 	return 0;
+# }
+#java
+# public class Main {
+
+
+#     public static void main (String[] args){
+
+#         int k = 10;
+
+#         System.out.println("nilai = " + k);
+
+
+#         if (k == 20){
+
+#             System.out.println("nilai true");
+
+#         } else {
+
+#             System.out.println("nilai false");
+
+#         }
+
+
+#         System.out.println("selesai");
+
+#     }
+# }
