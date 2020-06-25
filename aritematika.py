ecc3c81f6c591ba6844d33014412fa0040c668f7
miungbelajar <64505739+miungbelajar@users.noreply.github.com>
2020-06-25 09:30:27 +0700

    Create aritematika.py

---
+++ aritematika.py
@@ -0,0 +1,24 @@
+#operarasi aritematika
+a=5
+b=2
+#operasi tambah
+hasil=a+b
+print (a,'+',b,'=',hasil)
+#operasi pengurangan
+hasil=a-b
+print (a,'-',b,'=',hasil)
+#operasi pembagian
+hasil=a*b
+print (a,'*',b,'=',hasil)
+#operasi pembagian
+hasil=a/b
+print (a,'/',b,'=',hasil)
+#operasi pangkat
+hasil=a**b
+print (a,'**',b,'=',hasil)
+#operasi modulus
+hasil=a%b
+print (a,'%',b,'=',hasil)
+#operasi floor division
+hasil=a//b
+print (a,'//',b,'=',hasil)
